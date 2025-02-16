47ac01e22a5b1aed3ac1d7af5af7923ae85550a0
Utsav Choudhary <uchoudhary2005@hotmail.com>
2025-02-16 19:24:28 +0530

    Server and client commits

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
+const cors = require("cors")
+
+const authRoutes = require("./routes/auth.js")
+const listingRoutes = require('./routes/listing.js')
+const bookingRoutes = require('./routes/booking.js')
+const userRoutes = require('./routes/user.js')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use("/auth", authRoutes)
+app.use("/properties", listingRoutes)
+app.use("/bookings", bookingRoutes)
+app.use("/users", userRoutes)
+
+const PORT = 3001
+mongoose.connect(process.env.MONGO_URL, {
+    dbName: "WanderNest",
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
+.catch((err) => console.log(`${err} did not connect`));
