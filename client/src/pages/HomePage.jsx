4eba6c15776ee17421119bb1e0c0f92fd947d66a
Utsav Choudhary <uchoudhary2005@hotmail.com>
2025-05-05 15:30:49 +0530

    Home page routes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import  Navbar  from '../components/Navbar'
 import  Slide  from '../components/Slide'
 import Categories from '../components/Categories'
 import Listings from '../components/Listings'
-import Footer from '../components/Footer'
 
 const HomePage = () => {
   return (
