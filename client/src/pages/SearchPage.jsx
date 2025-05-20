53b02f4c246a17adba2bf590f49af9566323d767
Utsav Choudhary <uchoudhary2005@hotmail.com>
2025-05-20 20:37:08 +0530

    updates

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 import Loader from "../components/Loader"
 import Navbar from "../components/Navbar";
 import ListingCard from "../components/ListingCard";
-import Footer from "../components/Footer"
 
 const SearchPage = () => {
   const [loading, setLoading] = useState(true)
