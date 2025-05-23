db4657bfd4cec04572722097b595788957455635
Utsav Choudhary <uchoudhary2005@hotmail.com>
2025-05-23 23:00:42 +0530

    Wishlist page to display user's saved listings

--- client/src/pages/WishList.jsx
+++ client/src/pages/WishList.jsx
@@ -2,7 +2,6 @@ import "../styles/List.scss";
 import { useSelector } from "react-redux";
 import Navbar from "../components/Navbar";
 import ListingCard from "../components/ListingCard";
-
 import Footer from "../components/Footer"
 
 const WishList = () => {
