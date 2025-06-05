62f2dadbf42b58ab532834e928a3e24d349729b7
Utsav Choudhary <uchoudhary2005@hotmail.com>
2025-06-05 22:46:41 +0530

    Listing page details with booking summary and xalender

--- client/src/pages/ListingDetails.jsx
+++ client/src/pages/ListingDetails.jsx
@@ -53,10 +53,6 @@ const ListingDetails = () => {
   const end = new Date(dateRange[0].endDate);
   const dayCount = Math.max(1, Math.round((end - start) / (1000 * 60 * 60 * 24)));
 
-
-  const customerId = useSelector((state) => state?.user?._id);
-  const navigate = useNavigate();
-
   const handleSubmit = async () => {
     try {
       const bookingForm = {
